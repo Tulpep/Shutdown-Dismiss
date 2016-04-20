0b3c9236bb035903badca501cc97db7fc7c94c50
Ricardo Polo <ricardo.polo@tulpep.com>
2016-04-20 15:26:24 -0500

    All files added

---
+++ Tulpep.ShutdownDismiss/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Tulpep.ShutdownDismiss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1170, 238);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Su equipo se apagará automaticamente en 30 minutos.  \r\nDe este manera participo a" +
+    "ctivamente en el  cuidado del medio ambiente. \r\n\r\n¿Estoy de acuerdo  con apagar " +
+    "mi computador?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(388, 279);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Si";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(602, 279);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "No";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(275, 388);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(540, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "En caso de no recibir respuesta se apagará a las ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 444);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Apagar su computador";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tulpep.ShutdownDismiss/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Tulpep.ShutdownDismiss
+{
+    public partial class Form1 : Form
+    {
+
+        private const string SubkeyName = @"Software\Tulpep\ShutdownDismiss";
+        private const string KeyName = "NumberOfDismisses";
+        public Form1()
+        {
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            InitializeComponent();
+            //30 minutes
+            // Thise site helps in the conversion http://www.calculateme.com/Time/Minutes/ToMilliseconds.htm
+            int delayMiliseconds = 1800000;
+
+            var datetime = DateTime.Now.AddMilliseconds(delayMiliseconds);
+            label2.Text = label2.Text + datetime.ToString("hh:mm tt");
+
+            new Thread(() =>
+            {
+
+                Thread.CurrentThread.IsBackground = true;
+                Thread.Sleep(delayMiliseconds);
+                SetNumberOfDismisses(1);
+                Process.Start("shutdown", "/s /f /t 00");
+            }).Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            SetNumberOfDismisses(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numberOfDismisses  = GetNumberOfDismisses();
+            if (numberOfDismisses > 3)
+            {
+                MessageBox.Show("Usted ha cancelado el apagado diario por mas de tres dias. No lo puede cancelar de nuevo");
+            }
+            else
+            {
+                SetNumberOfDismisses(numberOfDismisses++);
+                this.Close();
+            }
+
+        }
+
+        private int GetNumberOfDismisses()
+        {
+            int existingNumber = 1;
+            using (var key = Registry.CurrentUser.CreateSubKey(SubkeyName))
+            {
+                var existing = (string) key.GetValue(KeyName);
+                if (existing != null)
+                {
+                    existingNumber = Int32.Parse(existing) + 1;
+                }
+            };
+            return existingNumber;
+        }
+
+        private void SetNumberOfDismisses(int number)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(SubkeyName))
+            {
+                key.SetValue(KeyName, number, RegistryValueKind.String);
+            };
+        }
+    }
+}
